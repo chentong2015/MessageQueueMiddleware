fb6810d2ee97164598fe92a9adc355a352117bac
Tong Chen <tong.chen@etu.utc.fr>
2022-01-09 20:05:25 +0100

    create all modules and first commit RabbitMQ

---
+++ JavaMessageService/src/main/java/main/BaseJMS.java
@@ -0,0 +1,13 @@
+package main;
+
+// Java Message Service
+// 一种中间件标准，JMS在特定的API接口层面和实现行为上进行了统一
+public class BaseJMS {
+
+    // JMS基础项目     https://github.com/Letitmiss/JMS
+    // Spring JMS项目 https://spring.io/guides/gs/messaging-jms/
+
+    // https://docs.oracle.com/javaee/6/tutorial/doc/bncdr.html
+    // https://www.javatpoint.com/jms-tutorial
+    // https://howtodoinjava.com/jms/jms-java-message-service-tutorial/
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # MessageQueueMiddleware
+
+该项目介绍消息中间件基础
+1. Apache ActiveMQ
+2. Apache Kafka
+3. Apache RocketMQ
+4. JMS(Java Message Service)
+5. RabbitMQ
