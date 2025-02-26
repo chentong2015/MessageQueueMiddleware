476c55926aba89e8503ae36221431b0efa69d5d1
chen tong <chen2015tong@gmail.com>
2025-02-26 14:54:39 +0100

    update message design

--- kafka-event-stream/src/main/java/producer_consumer/DemoKafkaConsumerProducer.java
+++ kafka-event-stream/src/main/java/producer_consumer/DemoKafkaConsumerProducer.java
@@ -27,35 +27,45 @@ public class DemoKafkaConsumerProducer {
     public static final String GROUP_NAME = "my-group";
 
     // TODO. 创建的Consumer和Producer都是异步线程，不会阻塞主线程
-    public static void main(String[] args) {
-        try {
-            if (args.length == 0) {
-                ConsoleUtils.printHelp("This example takes 2 parameters (i.e. 10000 sync):%n" +
+    public static void main(String[] args) throws InterruptedException {
+        if (args.length == 0) {
+            ConsoleUtils.printHelp("This example takes 2 parameters (i.e. 10000 sync):%n" +
                     "- records: total number of records to send (required)%n" +
                     "- mode: pass 'sync' to send records synchronously (optional)");
-                return;
-            }
+            return;
+        }
 
-            int numRecords = Integer.parseInt(args[0]);
-            boolean isAsync = args.length == 1 || !args[1].trim().equalsIgnoreCase("sync");
+        int numRecords = Integer.parseInt(args[0]);
+        boolean isAsync = args.length == 1 || !args[1].trim().equalsIgnoreCase("sync");
 
-            KafkaAdmin.recreateTopics(KafkaProperties.BOOTSTRAP_SERVERS, -1, TOPIC_NAME);
-            CountDownLatch latch = new CountDownLatch(2);
+        KafkaAdmin.recreateTopics(KafkaProperties.BOOTSTRAP_SERVERS, -1, TOPIC_NAME);
+        CountDownLatch latch = new CountDownLatch(2);
 
-            ProducerThread producerThread = new ProducerThread("producer", KafkaProperties.BOOTSTRAP_SERVERS,
-                    TOPIC_NAME, isAsync, null, false, numRecords, -1, latch);
-            producerThread.start();
-            ConsumerThread consumerThread = new ConsumerThread("consumer", KafkaProperties.BOOTSTRAP_SERVERS,
-                    TOPIC_NAME, GROUP_NAME, Optional.empty(), false, numRecords, latch);
-            consumerThread.start();
+        ProducerThread producerThread = new ProducerThread("producer",
+                KafkaProperties.BOOTSTRAP_SERVERS,
+                TOPIC_NAME,
+                isAsync,
+                null,
+                false,
+                numRecords,
+                -1,
+                latch);
+        producerThread.start();
 
-            if (!latch.await(5, TimeUnit.MINUTES)) {
-                ConsoleUtils.printErr("Timeout after 5 minutes waiting for termination");
-                producerThread.shutdown();
-                consumerThread.shutdown();
-            }
-        } catch (Throwable e) {
-            e.printStackTrace();
+        ConsumerThread consumerThread = new ConsumerThread("consumer",
+                KafkaProperties.BOOTSTRAP_SERVERS,
+                TOPIC_NAME,
+                GROUP_NAME,
+                Optional.empty(),
+                false,
+                numRecords,
+                latch);
+        consumerThread.start();
+
+        if (!latch.await(5, TimeUnit.MINUTES)) {
+            ConsoleUtils.printErr("Timeout after 5 minutes waiting for termination");
+            producerThread.shutdown();
+            consumerThread.shutdown();
         }
     }
 }
