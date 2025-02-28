dbe56eb808098ab0010213ce023834301c6bf4dd
chen tong <chen2015tong@gmail.com>
2025-02-28 18:43:47 +0100

    update Kafka design and rocketmq

---
+++ java-message-service-activemq/src/main/java/activemq/queue/MessageQueueConsumer.java
@@ -0,0 +1,31 @@
+package activemq.queue;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+public class MessageQueueConsumer {
+
+    private static final String URL = "http://127.0.0.1:61616";
+    private static final String QUEUE_NAME = "queue-test";
+
+    public static void main(String[] args) throws JMSException {
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(URL);
+        Connection connection = connectionFactory.createConnection();
+        connection.start();
+
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+        Destination destination = session.createQueue(QUEUE_NAME);
+        javax.jms.MessageConsumer consumer = session.createConsumer(destination);
+
+        consumer.setMessageListener(message -> {
+            TextMessage textMessage = (TextMessage) message;
+            try {
+                System.out.println("Received：" + textMessage.getText());
+            } catch (JMSException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+}
